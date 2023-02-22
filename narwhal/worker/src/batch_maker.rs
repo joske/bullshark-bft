26577be6772a2ebeaa6e64308921316d1c1b58cc
Jos Dehaes <jos.dehaes@gmail.com>
2023-02-22 20:46:59 +0100

    chore: remove benchmark feature

--- narwhal/worker/src/batch_maker.rs
+++ narwhal/worker/src/batch_maker.rs
@@ -11,9 +11,6 @@ use store::Store;
 use config::WorkerId;
 use tracing::error;
 
-#[cfg(feature = "benchmark")]
-use std::convert::TryInto;
-
 use futures::{Future, StreamExt};
 
 use mysten_metrics::spawn_logged_monitored_task;
@@ -173,55 +170,6 @@ impl BatchMaker {
         size: usize,
         responses: Vec<TxResponse>,
     ) -> Option<impl Future<Output = ()>> {
-        #[cfg(feature = "benchmark")]
-        {
-            let digest = batch.digest();
-
-            // Look for sample txs (they all start with 0) and gather their txs id (the next 8 bytes).
-            let tx_ids: Vec<_> = batch
-                .transactions
-                .iter()
-                .filter(|tx| tx[0] == 0u8 && tx.len() > 8)
-                .filter_map(|tx| tx[1..9].try_into().ok())
-                .collect();
-
-            for id in tx_ids {
-                // NOTE: This log entry is used to compute performance.
-                tracing::info!(
-                    "Batch {:?} contains sample tx {}",
-                    digest,
-                    u64::from_be_bytes(id)
-                );
-            }
-
-            #[cfg(feature = "trace_transaction")]
-            {
-                // The first 8 bytes of each transaction message is reserved for an identifier
-                // that's useful for debugging and tracking the lifetime of messages between
-                // Narwhal and clients.
-                let tracking_ids: Vec<_> = batch
-                    .transactions
-                    .iter()
-                    .map(|tx| {
-                        let len = tx.len();
-                        if len >= 8 {
-                            (&tx[0..8]).read_u64::<BigEndian>().unwrap_or_default()
-                        } else {
-                            0
-                        }
-                    })
-                    .collect();
-                tracing::debug!(
-                    "Tracking IDs of transactions in the Batch {:?}: {:?}",
-                    digest,
-                    tracking_ids
-                );
-            }
-
-            // NOTE: This log entry is used to compute performance.
-            tracing::info!("Batch {:?} contains {} B", digest, size);
-        }
-
         let reason = if timeout { "timeout" } else { "size_reached" };
 
         self.node_metrics
