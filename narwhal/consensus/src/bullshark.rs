311f0e7886237dc0eb50aa0fbc91e143bd063ff1
SplittyDev <splittydev@gmail.com>
2023-03-07 11:58:56 +0100

    Fix all clippy warnings

--- narwhal/consensus/src/bullshark.rs
+++ narwhal/consensus/src/bullshark.rs
@@ -26,7 +26,7 @@ pub mod bullshark_tests;
 pub struct LastRound {
     /// True when the leader has actually proposed a certificate
     /// and found in our DAG
-    leader_found: bool,
+    _leader_found: bool,
     /// When the leader has enough support from downstream
     /// certificates
     leader_has_support: bool,
@@ -100,7 +100,7 @@ impl ConsensusProtocol for Bullshark {
             Some(x) => x,
             None => {
                 self.last_leader_election = LastRound {
-                    leader_found: false,
+                    _leader_found: false,
                     leader_has_support: false,
                 };
                 // leader has not been found - we don't have any certificate
@@ -119,7 +119,7 @@ impl ConsensusProtocol for Bullshark {
             .sum();
 
         self.last_leader_election = LastRound {
-            leader_found: true,
+            _leader_found: true,
             leader_has_support: false,
         };
 
